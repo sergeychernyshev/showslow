996d89656961375f49f6f969b2f98c56b53fcc92
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-02-22 00:59:17 +0000

    Added Google Analytics code to beacon list page.

--- beacon/index.php
+++ beacon/index.php
@@ -1,6 +1,24 @@
-<html>
+<?php 
+require_once('../global.php');
+?><html>
 <head>
 <title>ShowSlow Beacons</title>
+<?php if ($googleAnalyticsProfile) {?>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '<?php echo $googleAnalyticsProfile ?>']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script');
+ga.src = ('https:' == document.location.protocol ?
+    'https://ssl' : 'http://www') +
+    '.google-analytics.com/ga.js';
+ga.setAttribute('async', 'true');
+document.documentElement.firstChild.appendChild(ga);
+})();
+</script>
+<?php }?>
 </head>
 <body>
 <h1>ShowSlow Beacons</h1>
