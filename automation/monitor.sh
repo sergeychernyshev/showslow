1ab9710ca95e67acc70975c3953fe19325ef9550
Aaron Kulick <aaron.kulick@gmail.com>
2010-03-31 01:47:40 +0000

    Fixed logic if there is no PID file... start Xvfb

--- automation/monitor.sh
+++ automation/monitor.sh
@@ -5,14 +5,17 @@ Xvfb_PIDFILE="/home/tmnz/__xvfb.pid"
 if [ -e "/home/tmnz/__xvfb.pid" ]; then
     Xvfb_PID=`cat $Xvfb_PIDFILE`
     if [ "`ps -eo pid | grep -c $Xvfb_PID`" != 1 ]; then
-        killall -e Xvfb
         Xvfb :1 -screen 0 1152x856x24 > /dev/null 2>&1 &
         Xvfb_PID=$!
         echo $Xvfb_PID > $Xvfb_PIDFILE
         echo "Xvfb Started: $Xvfb_PID => $Xvfb_PIDFILE"
     fi
+else
+    Xvfb :1 -screen 0 1152x856x24 > /dev/null 2>&1 &
+    Xvfb_PID=$!
+    echo $Xvfb_PID > $Xvfb_PIDFILE
+    echo "Xvfb Started: $Xvfb_PID => $Xvfb_PIDFILE"
 fi
-
 
 /home/tmnz/test_harness.pl --source http://example.com/testsuite.txt \
   --profile /home/tmnz/TMNZA \
