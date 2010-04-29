5bf80c07b84898257afb7e8ff03cfdcb3bc9013a
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-04-29 02:45:35 +0000

    Added YSlow and PageSpeed grades to my URLs page.

--- my.php
+++ my.php
@@ -4,25 +4,6 @@ require_once(dirname(__FILE__).'/users/users.php');
 
 $current_user = User::require_login();
 
-$query = sprintf("SELECT id, url, TIMEDIFF(now(), last_update) AS last FROM urls INNER JOIN user_urls ON urls.id = user_urls.url_id WHERE user_urls.user_id = %d ORDER BY url", $current_user->getID());
-
-$result = mysql_query($query);
-
-if (!$result) {
-	error_log(mysql_error());
-}
-
-$noMoreURLs = false;
-if ($maxURLsPerUser && mysql_num_rows($result) >= $maxURLsPerUser)
-{
-	$noMoreURLs = true;
-}
-
-$rows = array();
-while ($row = mysql_fetch_assoc($result)) {
-	$rows[] = $row;
-}
-
 if (array_key_exists('delete', $_POST) && is_array($_POST['delete'])) {
 	$delete = array_keys($_POST['delete']);
 
@@ -80,6 +61,30 @@ if (!$noMoreURLs && array_key_exists('url', $_REQUEST)
 	header('Location: '.$showslow_base.'my.php');
 }
 
+if ($current_user->getID() == 1) {
+	$maxURLsPerUser = false;
+}
+
+$query = sprintf("SELECT urls.id, urls.url, yslow2.o, pagespeed.o as ps_o, last_update FROM urls INNER JOIN user_urls ON urls.id = user_urls.url_id LEFT JOIN yslow2 on urls.yslow2_last_id = yslow2.id LEFT JOIN pagespeed on urls.pagespeed_last_id = pagespeed.id WHERE user_urls.user_id = %d ORDER BY url", $current_user->getID());
+
+$result = mysql_query($query);
+
+if (!$result) {
+	error_log(mysql_error());
+}
+
+$noMoreURLs = false;
+if ($maxURLsPerUser && mysql_num_rows($result) >= $maxURLsPerUser)
+{
+	$noMoreURLs = true;
+}
+
+$rows = array();
+while ($row = mysql_fetch_assoc($result)) {
+	$rows[] = $row;
+}
+
+
 require_once(dirname(__FILE__).'/header.php');
 ?>
 <h1 style="margin-bottom: 0">Add URLs to monitor</h1>
@@ -87,7 +92,7 @@ require_once(dirname(__FILE__).'/header.php');
 
 <form action="" method="POST">
 <table border="0">
-<tr><td colspan="4" style="padding-bottom: 1em">
+<tr><td colspan="8" style="padding-bottom: 1em">
 <?php
 if ($noMoreURLs)
 {
@@ -101,17 +106,38 @@ if ($noMoreURLs)
 </td></tr>
 
 <tr style="font-size: smaller; font-weight: bold">
+<td style="text-align: right; padding-right: 0.7em">Timestamp</td>
+<td colspan="2" style="text-align: right; padding-right: 0.7em">YSlow grade</td>
+<td colspan="2" style="text-align: right; padding-right: 0.7em">PageSpeed grade</td>
 <td style="text-align: center">Remove</td>
 <td style="padding-left: 1em">URL</td>
-<td style="text-align: right; padding-right: 0.7em">Time Since Last Check</td>
 </tr>
 
 <?php
 foreach ($rows as $row) {
 ?><tr>
-	<td style="text-align: center"><input type="submit" name="delete[<?php echo htmlentities($row['id'])?>]" value="X" title="Stop monitoring this URL" onclick="return confirm('Are you sure you want to remove this URL?')"/></td>
-	<td style="padding-left: 1em; overflow: hidden; white-space: nowrap;"><?php if ($row['last']) { ?><a href="details/?url=<?php echo urlencode($row['url'])?>"><?php } else { ?><i title="Time since last check for this URL"><?php } ?><?php echo htmlentities(substr($row['url'], 0, 100))?><?php if (strlen($row['url']) > 100) { ?>...<?php } ?><?php if ($row['last']) { ?></a><?php } else { ?></i><?php } ?></td>
-	<td style="text-align: right; padding-right: 1em"><?php echo $row['last'] ? '<a title="Time since last check for this URL" href="details/?url='.urlencode($row['url']).'">'.htmlentities($row['last']).'</span>' : '<i title="added to the testing queue">queued</i>'?></td>
+	<?php if ($row['last_update']) { ?>
+		<td style="text-align: right; padding-right: 1em"><a title="Time of last check for this URL" href="details/?url=<?php echo urlencode($row['url']); ?>"><?php echo htmlentities($row['last_update']); ?></a></td>
+		<?php if (!is_null($row['o'])) {?>
+			<td style="text-align: right; padding:0 10px 0 10px; white-space: nowrap;"><?php echo yslowPrettyScore($row['o'])?> (<?php echo $row['o']?>)</td>
+			<td><div style="background-color: silver; width: 101px" title="Current YSlow grade: <?php echo yslowPrettyScore($row['o'])?> (<?php echo $row['o']?>)"><div style="width: <?php echo $row['o']+1?>px; height: 0.7em; background-color: <?php echo scoreColor($row['o'])?>"/></div></td>
+		<?php } else { ?>
+			<td colspan="2"/>
+		<?php } ?>
+		<?php if (!is_null($row['ps_o'])) {?>
+			<td style="text-align: right; padding:0 10px 0 10px; white-space: nowrap;"><?php echo yslowPrettyScore($row['ps_o'])?> (<?php echo $row['ps_o']?>)</td>
+			<td><div style="background-color: silver; width: 101px" title="Current YSlow grade: <?php echo yslowPrettyScore($row['ps_o'])?> (<?php echo $row['ps_o']?>)"><div style="width: <?php echo $row['ps_o']+1?>px; height: 0.7em; background-color: <?php echo scoreColor($row['ps_o'])?>"/></div></td>
+		<?php } else { ?>
+			<td colspan="2"/>
+		<?php } ?>
+		<td style="text-align: center"><input type="submit" name="delete[<?php echo htmlentities($row['id'])?>]" value="X" title="Stop monitoring this URL" onclick="return confirm('Are you sure you want to remove this URL?')"/></td>
+		<td style="padding-left: 1em; overflow: hidden; white-space: nowrap;"><a href="details/?url=<?php echo urlencode($row['url'])?>"><?php echo htmlentities(substr($row['url'], 0, 100))?><?php if (strlen($row['url']) > 100) { ?>...<?php } ?></a></td>
+	<?php } else { ?>
+		<td style="text-align: right; padding-right: 1em"><i title="added to the testing queue">queued</i></td>
+		<td colspan="4"/>
+		<td style="text-align: center"><input type="submit" name="delete[<?php echo htmlentities($row['id'])?>]" value="X" title="Stop monitoring this URL" onclick="return confirm('Are you sure you want to remove this URL?')"/></td>
+		<td style="padding-left: 1em; overflow: hidden; white-space: nowrap;"><i title="Time of last check for this URL"><?php echo htmlentities(substr($row['url'], 0, 100))?><?php if (strlen($row['url']) > 100) { ?>...<?php } ?></i></td>
+	<?php } ?>
 </tr><?php
 }
 
