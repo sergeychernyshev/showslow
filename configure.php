b8e319eaf0bf1a238f3c6a0d7bdbc0795d068a65
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-05-02 14:17:21 +0000

    Added configuration documentation link to the wiki.

--- configure.php
+++ configure.php
@@ -117,7 +117,7 @@ document.documentElement.firstChild.appendChild(ga);
 		<p>
 		<p><b style="color: red">WARNING! Only use this if you're OK with all your measurements to be recorded by this instance of ShowSlow and displayed at <a href="<?php echo $showslow_base?>"><?php echo $showslow_base?></a><br/>You can also <a href="http://www.showslow.org/Installation_and_configuration">install ShowSlow on your own server</a> to limit the risk.</b></p>
 
-		<p>Set these two Firefox parameters on <b>about:config</b> page:</p>
+		<p>Set these Firefox parameters on <b>about:config</b> page:</p>
 		<h2>YSlow 2.x</h2>
 		<ul>
 		<li>extensions.yslow.beaconUrl = <b style="color: blue"><?php echo $showslow_base?>beacon/yslow/</b></li>
@@ -125,6 +125,8 @@ document.documentElement.firstChild.appendChild(ga);
 		<li>extensions.yslow.optinBeacon = <b style="color: blue">true</b></li>
 		</ul>
 		<h2>PageSpeed</h2>
+		<p>PageSpeed is configured to send metrics to <a href="http://www.showslow.com/">showslow.com</a> by default.</p>
+		<p>To send metrics to your instance located at <a href="<?php echo $showslow_base?>"><?php echo $showslow_base?></a>, set these Firefox parameters:</p>
 		<ul>
 		<li>extensions.PageSpeed.beacon.minimal.url = <b style="color: blue"><?php echo $showslow_base?>beacon/pagespeed/</b></li>
 		<li>extensions.PageSpeed.beacon.minimal.enabled = <b style="color: blue">true</b></li>
@@ -132,6 +134,12 @@ document.documentElement.firstChild.appendChild(ga);
 
 		<h2>More metrics</h2>
 		<p>For more information about different beacons supported by this instance of ShowSlow, see <a href="beacon/">beacons page</a></p>
+
+		<h2>Additional documentation</h2>
+		<p>You can find more detailed documentation on configuring tools to be sending data to Show Slow on our wiki here.</p>
+		<ul>
+			<li><a href="http://www.showslow.org/Tools_configuration">http://www.showslow.org/Tools_configuration</a></li>
+		</ul>
 	</div>
 	<div id="download">
 		<div class="progress">Loading...<br/><img src="progressbar.gif"/></div>
