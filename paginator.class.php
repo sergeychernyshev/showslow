b71a15327364c401394b5b5b957e5f0565c24f0a
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-06-29 22:20:42 +0000

    Oops. Forgot paginator.

---
+++ paginator.class.php
@@ -0,0 +1,109 @@
+<?php
+
+class Paginator{
+	var $items_per_page;
+	var $items_total;
+	var $current_page;
+	var $num_pages;
+	var $mid_range;
+	var $return;
+	var $default_ipp = 50;
+	var $querystring;
+
+	function Paginator()
+	{
+		$this->current_page = 1;
+		$this->mid_range = 7;
+		$this->items_per_page = $this->default_ipp;
+	}
+
+	function paginate()
+	{
+		$this->num_pages = ceil($this->items_total/$this->items_per_page);
+		$this->current_page = isset($_GET['page']) ? (int) $_GET['page'] : 1; // must be numeric > 0
+		if($this->current_page < 1 Or !is_numeric($this->current_page)) $this->current_page = 1;
+		if($this->current_page > $this->num_pages) $this->current_page = $this->num_pages;
+		$prev_page = $this->current_page-1;
+		$next_page = $this->current_page+1;
+
+		if($_GET)
+		{
+			$args = explode("&",$_SERVER['QUERY_STRING']);
+			foreach($args as $arg)
+			{
+				$keyval = explode("=",$arg);
+				if ($keyval[0] != "page") $this->querystring .= "&" . $arg;
+			}
+		}
+
+		if($_POST)
+		{
+			foreach($_POST as $key=>$val)
+			{
+				if($key != "page") $this->querystring .= "&$key=$val";
+			}
+		}
+
+		if($this->num_pages > 10)
+		{
+			$this->return = ($this->current_page != 1 And $this->items_total >= 10) ? "<a class=\"paginate\" href=\"$_SERVER[PHP_SELF]?page=$prev_page$this->querystring\">&laquo; Previous</a> ":"<span class=\"inactive\" href=\"#\">&laquo; Previous</span> ";
+
+			$this->start_range = $this->current_page - floor($this->mid_range/2);
+			$this->end_range = $this->current_page + floor($this->mid_range/2);
+
+			if($this->start_range <= 0)
+			{
+				$this->end_range += abs($this->start_range)+1;
+				$this->start_range = 1;
+			}
+			if($this->end_range > $this->num_pages)
+			{
+				$this->start_range -= $this->end_range-$this->num_pages;
+				$this->end_range = $this->num_pages;
+			}
+			$this->range = range($this->start_range,$this->end_range);
+
+			for($i=1;$i<=$this->num_pages;$i++)
+			{
+				if($this->range[0] > 2 And $i == $this->range[0]) $this->return .= " ... ";
+				// loop through all pages. if first, last, or in range, display
+				if($i==1 Or $i==$this->num_pages Or in_array($i,$this->range))
+				{
+					$this->return .= $i == $this->current_page ? "<a title=\"Go to page $i of $this->num_pages\" class=\"current\" href=\"#\">$i</a> ":"<a class=\"paginate\" title=\"Go to page $i of $this->num_pages\" href=\"$_SERVER[PHP_SELF]?page=$i$this->querystring\">$i</a> ";
+				}
+				if($this->range[$this->mid_range-1] < $this->num_pages-1 And $i == $this->range[$this->mid_range-1]) $this->return .= " ... ";
+			}
+			$this->return .= (($this->current_page != $this->num_pages And $this->items_total >= 10)) ? "<a class=\"paginate\" href=\"$_SERVER[PHP_SELF]?page=$next_page$this->querystring\">Next &raquo;</a>\n":"<span class=\"inactive\" href=\"#\">&raquo; Next</span>\n";
+		}
+		else
+		{
+			for($i=1;$i<=$this->num_pages;$i++)
+			{
+				$this->return .= ($i == $this->current_page) ? "<a class=\"current\" href=\"#\">$i</a> ":"<a class=\"paginate\" href=\"$_SERVER[PHP_SELF]?page=$i$this->querystring\">$i</a> ";
+			}
+		}
+	}
+
+	function display_jump_menu()
+	{
+		if ($this->num_pages <= 1) {
+			return '';
+		}
+
+		$option = '';
+
+		for($i=1;$i<=$this->num_pages;$i++)
+		{
+			$option .= ($i==$this->current_page) ? "<option value=\"$i\" selected>$i</option>\n":"<option value=\"$i\">$i</option>\n";
+		}
+		return "<span class=\"paginate\">Page:</span><select class=\"paginate\" onchange=\"window.location='$_SERVER[PHP_SELF]?page='+this[this.selectedIndex].value+'$this->querystring';return false\">$option</select>\n";
+	}
+
+	function display_pages()
+	{
+		if ($this->num_pages <= 1) {
+			return '';
+		}
+		return $this->return;
+	}
+}
