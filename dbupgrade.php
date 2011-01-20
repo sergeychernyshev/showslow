b552c55c0843e2dabcb930d47295f538a61b6d0c
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-01-20 04:31:23 +0000

    Moved initial data to 0->1 migration in dbupgrade.php

--- dbupgrade.php
+++ dbupgrade.php
@@ -390,6 +390,128 @@ $versions[2] = array(
 );
 
 // version 1
+// moved tables.sql here to unify upgrade and install process
+$versions[1]['up'][] = "CREATE TABLE `event` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `type` varchar(25) default NULL COMMENT 'string representing type of the event',
+  `url_prefix` blob NOT NULL COMMENT 'URL prefix to match the urls - usually protocol and host name',
+  `title` text NOT NULL COMMENT 'event message',
+  `start` timestamp NOT NULL default CURRENT_TIMESTAMP COMMENT 'start date of event',
+  `end` timestamp NULL default NULL COMMENT 'end date of event (if null, start is the same as end)',
+  `resource_url` blob COMMENT 'additional URL to resource related to the event.',
+  PRIMARY KEY  (`id`),
+  KEY `start` (`start`)
+) ENGINE=MyISAM";
+$versions[1]['up'][] = "CREATE TABLE `metric` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `url_id` bigint(20) unsigned NOT NULL default '0',
+  `metric_id` mediumint(8) unsigned NOT NULL default '0',
+  `value` float NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM";
+$versions[1]['up'][] = "CREATE TABLE `pagespeed` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `ip` int(4) unsigned NOT NULL default '0',
+  `user_agent` text NOT NULL,
+  `url_id` bigint(20) unsigned NOT NULL default '0',
+  `w` bigint(20) unsigned NOT NULL default '0',
+  `o` float unsigned NOT NULL default '0',
+  `l` bigint(20) unsigned NOT NULL default '0',
+  `r` smallint(6) unsigned NOT NULL default '0',
+  `t` bigint(20) unsigned NOT NULL default '0',
+  `v` text NOT NULL,
+  `pMinifyCSS` float unsigned NOT NULL default '0',
+  `pMinifyJS` float unsigned NOT NULL default '0',
+  `pOptImgs` float unsigned NOT NULL default '0',
+  `pImgDims` float unsigned NOT NULL default '0',
+  `pCombineJS` float unsigned NOT NULL default '0',
+  `pCombineCSS` float unsigned NOT NULL default '0',
+  `pCssInHead` float unsigned NOT NULL default '0',
+  `pBrowserCache` float unsigned NOT NULL default '0',
+  `pProxyCache` float unsigned NOT NULL default '0',
+  `pNoCookie` float unsigned NOT NULL default '0',
+  `pCookieSize` float unsigned NOT NULL default '0',
+  `pParallelDl` float unsigned NOT NULL default '0',
+  `pCssSelect` float unsigned NOT NULL default '0',
+  `pCssJsOrder` float unsigned NOT NULL default '0',
+  `pDeferJS` float unsigned NOT NULL default '0',
+  `pGzip` float unsigned NOT NULL default '0',
+  `pMinRedirect` float unsigned NOT NULL default '0',
+  `pCssExpr` float unsigned NOT NULL default '0',
+  `pUnusedCSS` float unsigned NOT NULL default '0',
+  `pMinDns` float unsigned NOT NULL default '0',
+  `pDupeRsrc` float unsigned NOT NULL default '0',
+  `pScaleImgs` float unsigned NOT NULL default '0' COMMENT 'Scale Images',
+  `pMinifyHTML` float unsigned NOT NULL default '0',
+  `pMinimizeRequestSize` float unsigned NOT NULL default '0',
+  `pOptimizeTheOrderOfStylesAndScripts` float unsigned NOT NULL default '0',
+  `pPutCssInTheDocumentHead` float unsigned NOT NULL default '0',
+  `pSpecifyCharsetEarly` float unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `url_id` (`url_id`)
+) ENGINE=MyISAM";
+$versions[1]['up'][] = "CREATE TABLE `urls` (
+  `id` bigint(20) unsigned NOT NULL auto_increment COMMENT 'id to reference',
+  `url` blob NOT NULL COMMENT 'url',
+  `last_update` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `last_event_update` timestamp NOT NULL default '0000-00-00 00:00:00' COMMENT 'Last time events were updated for this URL',
+  `w` bigint(20) unsigned NOT NULL default '0' COMMENT 'latest size of the page in bytes',
+  `o` smallint(6) unsigned default NULL COMMENT 'latest overall YSlow grade calculated for this profile',
+  `r` smallint(6) unsigned NOT NULL default '0' COMMENT 'latest amount of requests with empty cache',
+  `ps_w` bigint(20) unsigned NOT NULL default '0',
+  `ps_o` float unsigned default NULL,
+  `ps_l` bigint(20) unsigned NOT NULL default '0',
+  `ps_r` smallint(6) unsigned NOT NULL default '0',
+  `ps_t` bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `last_update` (`last_update`)
+) ENGINE=MyISAM";
+$versions[1]['up'][] = "CREATE TABLE `yslow2` (
+  `id` bigint(20) unsigned NOT NULL auto_increment COMMENT 'Entry id',
+  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP COMMENT 'Measurement timestamp',
+  `ip` int(4) unsigned NOT NULL default '0' COMMENT 'IP address of the agent',
+  `user_agent` text NOT NULL COMMENT 'User agent string',
+  `url_id` bigint(20) unsigned NOT NULL default '0',
+  `w` bigint(20) unsigned NOT NULL default '0' COMMENT 'size of the page in bytes',
+  `o` smallint(5) unsigned NOT NULL default '0' COMMENT 'overall YSlow grade calculated for this profile',
+  `r` smallint(6) unsigned NOT NULL default '0' COMMENT 'total amount of requests with empty cache',
+  `i` text NOT NULL COMMENT 'testing profile used',
+  `lt` bigint(20) unsigned NOT NULL default '0' COMMENT 'page load time',
+  `ynumreq` smallint(6) default NULL COMMENT 'Make fewer HTTP requests',
+  `ycdn` smallint(6) default NULL COMMENT 'Using CDN',
+  `yexpires` smallint(6) default NULL COMMENT 'Expires Headers',
+  `ycompress` smallint(6) default NULL COMMENT 'Gzip components',
+  `ycsstop` smallint(6) default NULL COMMENT 'CSS at the top',
+  `yjsbottom` smallint(6) default NULL COMMENT 'JS at the bottom',
+  `yexpressions` smallint(6) default NULL COMMENT 'CSS expressions',
+  `yexternal` smallint(6) default NULL COMMENT 'Make JavaScript and CSS external',
+  `ydns` smallint(6) default NULL COMMENT 'Reduce DNS lookups',
+  `yminify` smallint(6) default NULL COMMENT 'Minify JavaScript and CSS',
+  `yredirects` smallint(6) default NULL COMMENT 'Avoid URL redirects',
+  `ydupes` smallint(6) default NULL COMMENT 'Remove duplicate JavaScript and CSS',
+  `yetags` smallint(6) default NULL COMMENT 'Configure entity tags (ETags)',
+  `yxhr` smallint(6) default NULL COMMENT 'Make AJAX cacheable',
+  `yxhrmethod` smallint(6) default NULL COMMENT 'Use GET for AJAX requests',
+  `ymindom` smallint(6) default NULL COMMENT 'Reduce the number of DOM elements',
+  `yno404` smallint(6) default NULL COMMENT 'Avoid HTTP 404 (Not Found) error',
+  `ymincookie` smallint(6) default NULL COMMENT 'Reduce cookie size',
+  `ycookiefree` smallint(6) default NULL COMMENT 'Use cookie-free domains',
+  `ynofilter` smallint(6) default NULL COMMENT 'Avoid AlphaImageLoader filter',
+  `yimgnoscale` smallint(6) default NULL COMMENT 'Do not scale images in HTML',
+  `yfavicon` smallint(6) default NULL COMMENT 'Make favicon small and cacheable',
+  `details` text COMMENT 'Beacon details',
+  PRIMARY KEY  (`id`),
+  KEY `url_id` (`url_id`)
+) ENGINE=MyISAM";
+
+$versions[1]['down'][] = "DROP TABLE event";
+$versions[1]['down'][] = "DROP TABLE metric";
+$versions[1]['down'][] = "DROP TABLE pagespeed";
+$versions[1]['down'][] = "DROP TABLE urls";
+$versions[1]['down'][] = "DROP TABLE yslow2";
+
 // To get to version 1, use snapshot in tables.sql
 
 require_once(dirname(__FILE__).'/global.php');
