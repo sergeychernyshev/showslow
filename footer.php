8bc28a02199ec675a595581d600bbee5d7538c35
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-03-26 03:05:34 -0400

    Fixed a link to UserBase

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </div>
 <div id="footer">
-<div style="float: right">powered by <a href="http://code.google.com/p/userbase/">userbase</a></div>
+<div style="float: right">powered by <a target="_blank" href="http://github.com/sergeychernyshev/UserBase/">UserBase</a></div>
 &copy; 2010 Show Slow</div>
 </body></html>
