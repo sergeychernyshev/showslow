fe801a75af44d898fed953da097b38f21e596e91
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-07-23 11:45:36 -0400

    Updated a link to Startup API.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 	<div id="footer">
 		<div class="stackContent">
-			<p>&copy; 2010 Show Slow. Powered by <a target="_blank" href="http://www.showslow.org/">Show Slow</a> &amp; <a target="_blank" href="http://github.com/sergeychernyshev/UserBase/">User Base</a></p>
+			<p>&copy; 2010 Show Slow. Powered by <a target="_blank" href="http://www.showslow.org/">Show Slow</a> &amp; <a target="_blank" href="http://startupapi.com/">Startup API</a></p>
 		</div><!-- stackContent -->
 	</div><!-- footer -->
 </div><!-- stack -->
