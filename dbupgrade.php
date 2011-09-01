2527981192fe9ae0f7d77a8c228f0e1f71a7beb6
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-09-01 18:20:24 -0400

    Switching main URLs table to INNODB to reduce congestion.

--- dbupgrade.php
+++ dbupgrade.php
@@ -12,6 +12,13 @@ $versions = array();
 // Add new migrations on top, right below this line.
 
 /* -------------------------------------------------------------------------------------------------------
+ * VERSION 30
+ * Switching urls table to INNODB engine for better performance
+*/
+$versions[30]['up'][] = 'ALTER TABLE urls ENGINE = INNODB';
+$versions[30]['down'][] = 'ALTER TABLE urls ENGINE = MyISAM';
+
+/* -------------------------------------------------------------------------------------------------------
  * VERSION 29
  * Switching to INNODB engine for better performance
 */
