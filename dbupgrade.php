47d1de5a4a70e006909368f1226af5abb0e3f043
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-09-01 18:16:53 -0400

    Try to releave locking problem.

--- dbupgrade.php
+++ dbupgrade.php
@@ -12,6 +12,13 @@ $versions = array();
 // Add new migrations on top, right below this line.
 
 /* -------------------------------------------------------------------------------------------------------
+ * VERSION 29
+ * Switching to INNODB engine for better performance
+*/
+$versions[29]['up'][] = 'ALTER TABLE user_urls ENGINE = INNODB';
+$versions[29]['down'][] = 'ALTER TABLE user_urls ENGINE = MyISAM';
+
+/* -------------------------------------------------------------------------------------------------------
  * VERSION 28
  * Adding more WebPageTest fields
 */
