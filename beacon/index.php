e799ae702aeb8a8f523e2a0a054b90f132809df3
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-12-25 02:01:39 -0400

    Added WPT beacon to a list of beacons

--- beacon/index.php
+++ beacon/index.php
@@ -10,6 +10,7 @@ This instance of ShowSlow supports following beacons:
 <ul>
 <li><a href="yslow/">YSlow beacon</a></li>
 <li><a href="pagespeed/">Page Speed beacon</a></li>
+<li><a href="webpagetest/">WebPageTest beacon</a></li>
 <li><a href="events/">Custom events beacon</a></li>
 <li><a href="metric/">Custom metrics beacon</a></li>
 <li><a href="har/">HAR beacon</a></li>
