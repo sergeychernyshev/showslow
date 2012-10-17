9688e72c6e3a6e9723686aeef1040789e6086a56
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-10-16 22:03:13 -0400

    Script to execute phantomjs version of YSlow

---
+++ automation/phantomjs/monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SHOWSLOWBASE='... put your instance URL here ...' # e.g. 'http://www.showslow.com'
+
+URLS=`wget $SHOWSLOWBASE/monitor.php -O - -q`
+
+# Download and install PhantomJS: http://phantomjs.org/download.html
+PHANTOMJS='... path to phantomjs executable ...'
+
+# Download the latest build of phantomjs version of yslow: https://github.com/marcelduran/yslow/downloads
+YSLOWJS='... path to yslow.js ...'
+
+for URL in $URLS
+do
+	echo "$URL" | $PHANTOMJS $YSLOWJS -i grade -b $SHOWSLOWBASE/beacon/yslow/ >/dev/null
+done
+
